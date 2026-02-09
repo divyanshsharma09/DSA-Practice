c520b3b09e24dbd0f969d31dd5241716600e466d
Divyansh Sharma <divyansh.sharma788021@gmail.com>
2026-02-09 18:22:00 +0530

    Find Maximum Element problem
    Implemented a Java program to find the maximum element in an array by traversing and comparing values.

---
+++ Arrays/Find_Max_Element.java
@@ -0,0 +1,26 @@
+/*
+Problem: Find Maximum Element in Array
+Approach: Traverse array and compare values
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+import java.util.Scanner;
+
+public class maximum {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        int max = arr[0];
+        for(int i = 0; i<n; i++){
+            if(arr[i]>max){
+                max = arr[i];
+            }
+        }
+        System.out.println(max);
+        sc.close();
+    }
+}
