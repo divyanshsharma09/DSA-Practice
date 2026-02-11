3aa7839055b2cb67c96017bf1f5187ac9835e2d5
Divyansh Sharma <divyansh.sharma788021@gmail.com>
2026-02-11 21:19:32 +0530

    Add Longest Subarray With Sum ≤ K solution
    Implement solution for finding the longest subarray with a sum less than or equal to K using a sliding window approach.

---
+++ Arrays/Longest_Subarray_Sum_K.java
@@ -0,0 +1,39 @@
+/*
+Problem: Longest Subarray With Sum ≤ K
+Approach:
+    Expand window by adding elements
+    Shrink window if sum becomes greater than K
+Time Complexity: O(n)
+Space Complexity: O(1)
+Author Divyansh Sharma
+*/
+
+import java.util.Scanner;
+public class longest_Subarray {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int [] arr = new int[n];
+        for(int i = 0; i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int k = sc.nextInt();
+
+        int window_sum = 0;
+        int maxlength = 0;
+        int left = 0;
+
+        for(int right = 0 ;right<n;right++){
+            window_sum += arr[right];
+            while(window_sum>k){
+                window_sum -= arr[left];
+                left++;
+            }
+            maxlength = Math.max(maxlength,right-left+1);
+
+        }
+        System.out.println("Max length on sum is :"+maxlength);
+        sc.close();
+    }
+}
